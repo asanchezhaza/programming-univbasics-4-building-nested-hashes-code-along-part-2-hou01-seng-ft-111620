fbf3ba92bc65bb18634d6766783d74ae34d58069
Alan sanchez <asanchezhaza@gmail.com>
2020-10-18 09:06:50 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -53,9 +53,9 @@ def add_characters
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       },
-      :additional_characters =>{ [{name: "Prince Escalus"}
+      :additional_characters=  [{name: "Prince Escalus"}
                                   {name:"Apothecary"}]
-    }
+    
      
    }
 
